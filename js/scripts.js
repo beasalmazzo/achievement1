335c717e905c00b4c3459795a7899af5880f86a2
beasalmazzo <beatrizsalmazzo@gmail.com>
2022-12-26 23:11:39 +0100

    print pokemons list and their heights
    correct variable declaration

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
-pokemonList = [];
 
-let pokemonList = [
+/*on Exercise 2 the declaration of the variable was incorrect, so I fixed it now.
+Before:
+pokemonList = []; 
+
+let pokemonList = [  ------------ 'let' in the incorrect line
+
+*/
+let pokemonList = [];
+
+pokemonList = [
     { name: 'Charmander', height: 0.6, type: ['fire']},
     { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
     { name: 'Squirtle', height: 0.5, type: ['water']}
 ];
+
+//write on the screen a list of pokemons and their heights
+for (i=0 ; i<pokemonList.length ; i++){
+    if (pokemonList[i].height > 0.6) {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!\n`);
+        // why is \n acting like a space instead of an enter?
+    }   
+    else {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})\n`);
+    }
+}
+    
