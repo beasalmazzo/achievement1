ec71afef95226a5d546964b550eb4c020775c9e3
beasalmazzo <beatrizsalmazzo@gmail.com>
2022-12-26 20:09:24 +0100

    correct typo

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,5 @@ pokemonList = [];
 let pokemonList = [
     { name: 'Charmander', height: 0.6, type: ['fire']},
     { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    { name: 'Squirtle', height: 0.5, type: ['water']},
+    { name: 'Squirtle', height: 0.5, type: ['water']}
 ];
