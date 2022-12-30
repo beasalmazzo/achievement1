b9165741b90551664a012df9b17582c1c11e86b2
beasalmazzo <beatrizsalmazzo@gmail.com>
2022-12-30 18:30:28 +0100

    add close button to modal
    added button to close the modal when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -33,66 +33,76 @@ let pokemonRepository = (function() {
             //modal goes here
             //////////////////////////////
 
+            (function() {
 
+                //pops up the modal by adding class is-visible
+                let modalContainer = document.querySelector('#modal-container');
+                modalContainer.innerHTML = '';
+                modalContainer.classList.add('is-visible');
+
+                //create modal div
+                let modal = document.createElement('div');
+                modal.setAttribute('id', 'modal');
+                modalContainer.appendChild(modal);
+                
+                ///create structuce inside modal (all /// comments are part of this 'group')
+                ///create close button
+                let closeButtonElement = document.createElement('button');
+                closeButtonElement.classList.add('modal-close');
+                modal.appendChild(closeButtonElement);
+                closeButtonElement.innerText = 'Close';
+                closeButtonElement.addEventListener('click', () => {
+                    modalContainer.classList.remove('is-visible');
+                });
+                
+                ///insert pokemon name
+                let titleElement = document.createElement('h1');
+                titleElement.setAttribute('id', 'title'); //not necessary because of above line. Unless I want to use the id for styling
+                modal.appendChild(titleElement);
+                titleElement.innerText = pokemon.name;
+                
+                ///create image container
+                let imageContainer = document.createElement('div');
+                imageContainer.setAttribute('id', 'image-container');
+                modal.appendChild(imageContainer);
+                
+                ///add pokemon image to modal
+                let imageElement = document.createElement('img');
+                imageElement.setAttribute('id', 'pokemon-picture');
+                imageContainer.appendChild(imageElement);
+                imageElement.setAttribute('src', pokemon.imageUrl);
+                imageElement.setAttribute('alt', 'picture of the selected pokemon');
+                
+                ///insert pokemon details
+                let contentElement = document.createElement('div');
+                contentElement.setAttribute('id', 'pokemon-details');
+                modal.appendChild(contentElement);
+                contentElement.innerText = 'Height: ' + pokemon.height/10 +  ' m\nTypes: ' + newTypes(pokemon);
+
+                ///return types of pokemons (because pokemon.types is an object)
+                function newTypes (pokemon){
+
+                    let type1 = pokemon.types[0].type;
+
+                    if (pokemon.types.length > 1){
+                        let type2 = pokemon.types[1].type;
+                        return `${type1.name} / ${type2.name}`;
+                    }
+                    else{
+                        return type1.name;
+                    }
+                }
+
+                
+
+                
+
+                console.log(pokemon);
+
+            })();
                
 
-                (function() {
-
-                    //pops up the modal by adding class is-visible
-                    let modalContainer = document.querySelector('#modal-container');
-                    modalContainer.innerHTML = '';
-                    modalContainer.classList.add('is-visible');
-
-                    //create modal div
-                    let modal = document.createElement('div');
-                    modal.setAttribute('id', 'modal');
-                    modalContainer.appendChild(modal);
-                    
-                    //create structuce inside modal
-                    //insert pokemon name
-                    let titleElement = document.createElement('h1');
-                    titleElement.setAttribute('id', 'title'); //not necessary because of above line. Unless I want to use the id for styling
-                    modal.appendChild(titleElement);
-                    titleElement.innerText = pokemon.name;
-                    //create image container
-                    let imageContainer = document.createElement('div');
-                    imageContainer.setAttribute('id', 'image-container');
-                    modal.appendChild(imageContainer);
-                    //add pokemon image to modal
-                    let imageElement = document.createElement('img');
-                    imageElement.setAttribute('id', 'pokemon-picture');
-                    imageContainer.appendChild(imageElement);
-                    imageElement.setAttribute('src', pokemon.imageUrl);
-                    imageElement.setAttribute('alt', 'picture of the selected pokemon');
-                    //insert pokemon details
-                    let contentElement = document.createElement('p');
-                    contentElement.setAttribute('id', 'pokemon-details');
-                    modal.appendChild(contentElement);
-                    contentElement.innerText = 'Height: ' + pokemon.height +  '\nTypes: ' + newTypes(pokemon);
-
-                    function newTypes (pokemon){
-
-                        let type1 = pokemon.types[0].type;
-                        if (pokemon.types.length > 1){
-                            let type2 = pokemon.types[1].type;
-                            return `${type1.name} / ${type2.name}`;
-                        }
-                        else{
-                            return type1.name;
-                        }
-
-                    }
-                    newTypes (pokemon);
-
-                    /*document.createElement('button');
-                    let closeButtonElement = document.querySelector('button');
-                    closeButtonElement.classList.add('close-button');*/
-                    
-                    
-
-                    console.log(pokemon);
-
-                })();
+               
 
 
 
